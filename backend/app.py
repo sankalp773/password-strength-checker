b2411d8bc08658bc288e478f959d4c8e62c35fd9
sankalp773 <75838444+sankalp773@users.noreply.github.com>
2025-06-24 17:12:46 +0530

    Made the server communicate to each other and added dummy logic

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,27 @@
-from flask import Flask
+from flask import Flask , request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app) #allow request from frontend
 
 @app.route("/")
 
 def home():
 	return "Welcome to the Password Strength Checker!"
 
+@app.route("/check-password", methods = ["POST"])
+
+def check_password() :
+	data = request.get_json()
+	password = data.get("password","")
+
+	#dummy logic for now
+	if len(password) < 6 :
+		feedback  = "Too Short ! add more characters."
+	else :
+		feedback = "Looks okay, will improve this."
+
+	return jsonify({"feedback":feedback})
 
 @app.route("/ping")
 def ping():
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Welcome to the Password Strength Checker.
-        </p>
-      </header>
+function App(){
+  const [password,setPassword] = useState("");
+  const [feedback,setFeedback] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+//Send POST request to Flask Backend.
+
+    try {
+      const response = await fetch ("http://localhost:5000/check-password", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({password}),
+      });
+
+      const data  = await response.json();
+      setFeedback(data.feedback);
+    } catch(error) {
+      console.error("Error:", error);
+      setFeedback("Server error . please try again. "); 
+      }
+  };
+
+  return(
+    <div style={{padding: "2rem",fontFamily: "sans-serif"}}>
+      <h1>Password Strength Checker </h1>
+      <form onSubmit={handleSubmit}>
+        <input 
+        type='password' 
+        placeholder='Enter Your Password' 
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        style={{
+          padding: "0.5rem",
+          fontSize: "1rem",
+          width:"300px",
+          marginRight: "1rem",
+        }}
+        />
+        <button type='submit' style={{padding: " 0.5rem  1rem "}}>
+          Check
+        </button>
+      </form>
+      <div style={{marginTop: "1rem",fontWeight: "bold"}}>
+        {feedback && <p>{feedback}</p>}
+      </div>
     </div>
   );
 }
