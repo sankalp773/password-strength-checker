414907445129423a543fd16a8e20272a825328a4
sankalp773 <75838444+sankalp773@users.noreply.github.com>
2025-06-25 21:09:23 +0530

    added default port for hosting

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from zxcvbn import zxcvbn
 
 app = Flask(__name__)
-CORS(app) #allow request from frontend
+CORS(app) #allow request from frontend	
 
 @app.route("/")
 def home():
@@ -53,4 +53,4 @@ def check_password() :
 
 
 if __name__ == "__main__":
-	app.run(debug = True)
+	app.run(debug = True,host="0.0.0.0", port=10000)
